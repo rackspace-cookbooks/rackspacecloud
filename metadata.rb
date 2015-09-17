f4677fbb0fd50799ce6c978c1572184f5e4dc0dd
lmunro <lachlan.munro@gmail.com>
2015-09-17 18:26:17 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ryan.walker@rackspace.com"
 license          "Apache 2.0"
 description      "Provides LWRP's for managing Rackspace Cloud resources." 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 
 depends "xml"
